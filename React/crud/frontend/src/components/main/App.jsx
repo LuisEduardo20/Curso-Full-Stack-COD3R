6ef4d3f10f80bdd3d77adb48b5a8975ba7ef8119
Luis Eduardo Santos Costa <luiseduardo70@live.com>
2021-10-17 23:15:09 -0300

    projetos do capitulo de React

--- React/crud/frontend/src/components/main/App.jsx
+++ React/crud/frontend/src/components/main/App.jsx
@@ -1,15 +1,18 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+
 import React from 'react';
 
 import Logo from '../template/logo/Logo';
 import Nav from '../template/nav/Nav';
-import Main from '../template/main/Main';
+import Home from '../home/Home';
 import Footer from '../template/footer/Footer'
 
 export default props =>
     <div className="app">
         <Logo />
         <Nav />
-        <Main />
+        <Home />
         <Footer />
     </div>
